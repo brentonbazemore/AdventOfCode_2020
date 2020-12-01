cb08e037a367bbabca57ab9961ba79f2fee61421
Brenton Bazemore <brenton.bazemore@slalom.com>
2020-11-30 19:27:03 -0500

    Add next part helper script

--- newDay.sh
+++ newDay.sh
@@ -3,12 +3,11 @@ FOLDER_NAME=$1
 if [ -d $FOLDER_NAME ] 
 then
   echo "$FOLDER_NAME already exists."
-  exit 1
+  return 1
 fi
 
 mkdir $FOLDER_NAME
-
 cp -R ./NodeTypescriptStarterTemplate ./$FOLDER_NAME/pt1
-
 git add .
 git commit -m "Init $FOLDER_NAME - pt1"
+cd $FOLDER_NAME/pt1
