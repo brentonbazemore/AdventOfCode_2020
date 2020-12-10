8ce040843a89594f5df98250a1cb8118c7ad78b8
Brenton Bazemore <brenton.bazemore@slalom.com>
2020-12-10 16:51:18 -0500

    Finish d10 - pt2

--- d10/pt2/dist/index.js
+++ d10/pt2/dist/index.js
@@ -18,23 +18,40 @@ var __importStar = (this && this.__importStar) || function (mod) {
     __setModuleDefault(result, mod);
     return result;
 };
+var __spreadArrays = (this && this.__spreadArrays) || function () {
+    for (var s = 0, i = 0, il = arguments.length; i < il; i++) s += arguments[i].length;
+    for (var r = Array(s), k = 0, i = 0; i < il; i++)
+        for (var a = arguments[i], j = 0, jl = a.length; j < jl; j++, k++)
+            r[k] = a[j];
+    return r;
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 var fs = __importStar(require("fs"));
 var rawData = fs.readFileSync('input.txt', 'utf8');
 var data = rawData.split('\n');
-var sorted = data.sort(function (a, b) { return +a - +b; });
-var ones = 1;
-var threes = 0;
-sorted.forEach(function (s, i) {
-    if (i > sorted.length) {
-        return;
+var sorted = data.sort(function (a, b) { return +a - +b; }).map(function (s) { return +s; });
+var expectedEnd = sorted[sorted.length - 1];
+var plugs = __spreadArrays([0], sorted, [expectedEnd + 3]);
+var memo = new Map();
+var findNext = function (curIndex) {
+    if (memo.has(curIndex)) {
+        return memo.get(curIndex);
     }
-    var next = +sorted[i + 1];
-    if ((+s - next) === -1) {
-        ones++;
+    var currentJolts = plugs[curIndex];
+    var validLeaves = 0;
+    for (var i = 1; i <= 3; i++) {
+        var nextJolts = plugs[curIndex + i];
+        if (nextJolts - currentJolts <= 3) {
+            if (nextJolts === expectedEnd + 3) {
+                validLeaves++;
+                continue;
+            }
+            var out = findNext(curIndex + i);
+            validLeaves += out;
+        }
     }
-    else {
-        threes++;
-    }
-});
-console.log({ ones: ones, threes: threes }, ones * threes);
+    memo.set(curIndex, validLeaves);
+    return validLeaves;
+};
+var count = findNext(0);
+console.log({ count: count });
--- d10/pt2/index.ts
+++ d10/pt2/index.ts
@@ -3,21 +3,36 @@ import * as fs from 'fs';
 const rawData: string = fs.readFileSync('input.txt', 'utf8');
 const data: string[] = rawData.split('\n');
 
-const sorted = data.sort((a, b) => +a - +b);
+const sorted = data.sort((a, b) => +a - +b).map(s => +s);
+const expectedEnd = sorted[sorted.length - 1];
 
-let ones = 1;
-let threes = 0;
-
-sorted.forEach((s, i) => {
-  if (i > sorted.length) {
-    return;
+const plugs = [0, ...sorted, expectedEnd + 3];
+const memo = new Map();
+const findNext = (curIndex: number) => {
+  if (memo.has(curIndex)) {
+    return memo.get(curIndex);
   }
-  const next = +sorted[i + 1];
-  if ((+s - next) === -1) {
-    ones++;
-  } else {
-    threes++;
-  }
-});
+  const currentJolts = plugs[curIndex];
 
-console.log({ ones, threes }, ones * threes)
+  let validLeaves = 0;
+  for (let i = 1; i <= 3; i++) {
+    const nextJolts = plugs[curIndex + i];
+
+    if (nextJolts - currentJolts <= 3) {
+      if (nextJolts === expectedEnd + 3) {
+        validLeaves++;
+        continue;
+      }
+
+      const out = findNext(curIndex + i);
+      validLeaves += out;
+    }
+  }
+
+  memo.set(curIndex, validLeaves);
+
+  return validLeaves;
+};
+
+const count = findNext(0);
+console.log({ count });
