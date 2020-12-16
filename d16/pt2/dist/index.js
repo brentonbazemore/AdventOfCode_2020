159918b3e45ad061754efc363499b9cb77d60f50
Brenton Bazemore <brenton.bazemore@slalom.com>
2020-12-16 17:50:09 -0500

    Finish d16 - pt2

--- d16/pt2/dist/index.js
+++ d16/pt2/dist/index.js
@@ -20,8 +20,10 @@ var __importStar = (this && this.__importStar) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 var fs = __importStar(require("fs"));
+var _ = __importStar(require("lodash"));
 var rawData = fs.readFileSync('input.txt', 'utf8');
 var _a = rawData.split('\n\n'), rulesData = _a[0], yourTickeData = _a[1], nearbyTicketData = _a[2];
+//
 var rules = rulesData.split('\n').map(function (ru) {
     var _a = ru.split(': '), field = _a[0], rawRanges = _a[1];
     var ranges = rawRanges.split(' or ').map(function (ra) {
@@ -30,16 +32,54 @@ var rules = rulesData.split('\n').map(function (ru) {
     });
     return { field: field, ranges: ranges };
 });
+//
+var yourTicket = yourTickeData.split('\n').pop().split(',').map(function (n) { return +n; });
 var rawNearbyTickets = nearbyTicketData.split('\n');
 rawNearbyTickets.shift();
 var nearbyTickets = rawNearbyTickets.map(function (v) { return v.split(',').map(function (n) { return +n; }); });
-var invalid = [];
-nearbyTickets.forEach(function (ticket) {
-    ticket.forEach(function (num) {
-        var isValid = rules.some(function (rule) { return rule.ranges.some(function (range) { return num >= range.low && num <= range.high; }); });
-        if (!isValid) {
-            invalid.push(num);
+//
+var validTickets = nearbyTickets.filter(function (ticket) { return ticket.every(function (num) { return rules.some(function (rule) { return rule.ranges.some(function (range) { return num >= range.low && num <= range.high; }); }); }); });
+var determinePossibleFieldByIndex = function (index) {
+    var fieldArrays = validTickets.map(function (ticket) {
+        var num = ticket[index];
+        var validRules = rules.filter(function (rule) { return rule.ranges.some(function (range) { return num >= range.low && num <= range.high; }); });
+        return validRules.map(function (r) { return r.field; });
+    });
+    var fields = _.intersection.apply(_, fieldArrays);
+    return { index: index, fields: fields };
+};
+var indices = _.range(0, yourTicket.length);
+var confirmedIndices = {};
+var confirmedFields = [];
+var candidateFields = [];
+indices.forEach(function (index) {
+    var out = determinePossibleFieldByIndex(index);
+    if (out.fields.length === 1) {
+        confirmedFields.push(out.fields[0]);
+        confirmedIndices[out.fields[0]] = index;
+    }
+    else {
+        candidateFields.push(out);
+    }
+});
+while (confirmedFields.length !== indices.length) {
+    candidateFields = candidateFields.filter(function (obj, i) {
+        var remainingFields = _.difference(obj.fields, confirmedFields);
+        if (remainingFields.length === 1) {
+            confirmedFields.push(remainingFields[0]);
+            confirmedIndices[remainingFields[0]] = obj.index;
+            return false;
+        }
+        else {
+            candidateFields[i].fields = remainingFields;
+            return true;
         }
     });
-});
-console.log(invalid.reduce(function (prev, cur) { return prev + cur; }, 0));
+}
+var result = yourTicket[confirmedIndices['departure location']]
+    * yourTicket[confirmedIndices['departure station']]
+    * yourTicket[confirmedIndices['departure platform']]
+    * yourTicket[confirmedIndices['departure track']]
+    * yourTicket[confirmedIndices['departure date']]
+    * yourTicket[confirmedIndices['departure time']];
+console.log(result);
--- d16/pt2/index.ts
+++ d16/pt2/index.ts
@@ -1,4 +1,5 @@
 import * as fs from 'fs';
+import * as _ from 'lodash';
 
 const rawData: string = fs.readFileSync('input.txt', 'utf8');
 const [rulesData, yourTickeData, nearbyTicketData] = rawData.split('\n\n');
@@ -11,6 +12,7 @@ interface Rule {
   }[]
 }
 
+//
 const rules: Rule[] = rulesData.split('\n').map((ru) => {
   const [field, rawRanges] = ru.split(': ');
   const ranges = rawRanges.split(' or ').map((ra) => {
@@ -21,18 +23,62 @@ const rules: Rule[] = rulesData.split('\n').map((ru) => {
   return { field, ranges };
 });
 
+//
+const yourTicket = yourTickeData.split('\n').pop()!.split(',').map(n => +n);
+
 const rawNearbyTickets = nearbyTicketData.split('\n');
 rawNearbyTickets.shift();
 const nearbyTickets = rawNearbyTickets.map(v => v.split(',').map(n => +n));
 
-const invalid: number[] = [];
-nearbyTickets.forEach((ticket) => {
-  ticket.forEach((num) => {
-    const isValid = rules.some(rule => rule.ranges.some(range => num >= range.low && num <= range.high))
-    if (!isValid) {
-      invalid.push(num);
-    }
-  })
+//
+const validTickets = nearbyTickets.filter((ticket) => ticket.every((num) => rules.some(rule => rule.ranges.some(range => num >= range.low && num <= range.high))));
+
+const determinePossibleFieldByIndex = (index: number) => {
+  const fieldArrays = validTickets.map(ticket => {
+    const num = ticket[index];
+    const validRules = rules.filter(rule => rule.ranges.some(range => num >= range.low && num <= range.high));
+    return validRules.map(r => r.field);
+  });
+
+  const fields = _.intersection(...fieldArrays);
+  return { index, fields };
+};
+
+const indices = _.range(0, yourTicket.length);
+
+const confirmedIndices: { [key: string]: number } = {};
+const confirmedFields: string[] = [];
+let candidateFields: { index: number, fields: string[] }[] = [];
+indices.forEach(index => {
+  const out = determinePossibleFieldByIndex(index);
+  if (out.fields.length === 1) {
+    confirmedFields.push(out.fields[0]);
+    confirmedIndices[out.fields[0]] = index;
+  } else {
+    candidateFields.push(out);
+  }
 });
 
-console.log(invalid.reduce((prev, cur) => prev + cur, 0));
+while (confirmedFields.length !== indices.length) {
+  candidateFields = candidateFields.filter((obj, i) => {
+    const remainingFields = _.difference(obj.fields, confirmedFields);
+
+    if (remainingFields.length === 1) {
+      confirmedFields.push(remainingFields[0]);
+      confirmedIndices[remainingFields[0]] = obj.index;
+      return false;
+    } else {
+      candidateFields[i].fields = remainingFields;
+      return true;
+    }
+  });
+}
+
+const result = yourTicket[confirmedIndices['departure location']]
+  * yourTicket[confirmedIndices['departure station']]
+  * yourTicket[confirmedIndices['departure platform']]
+  * yourTicket[confirmedIndices['departure track']]
+  * yourTicket[confirmedIndices['departure date']]
+  * yourTicket[confirmedIndices['departure time']];
+
+console.log(result);
