14aa9dfb6f512837eb1d598d1895133eb4270bd9
Brenton Bazemore <brenton.bazemore@slalom.com>
2020-12-17 11:23:29 -0500

    Finish d17 - pt2

--- d17/pt2/dist/index.js
+++ d17/pt2/dist/index.js
@@ -18,7 +18,7 @@ var __importStar = (this && this.__importStar) || function (mod) {
     __setModuleDefault(result, mod);
     return result;
 };
-var _a, _b;
+var _a, _b, _c;
 Object.defineProperty(exports, "__esModule", { value: true });
 var fs = __importStar(require("fs"));
 var logDimension = function (dim) {
@@ -43,63 +43,71 @@ var bounds = {
     x: [-1, data[0].length],
     y: [-1, data.length],
     z: [-1, 1],
+    w: [-1, 1],
 };
-var pocketDimension = [data];
+var pocketDimension = [[data]];
 // logDimension(pocketDimension);
-var checkState = function (x, y, z) {
-    var _a, _b;
-    var state = (_b = (_a = pocketDimension === null || pocketDimension === void 0 ? void 0 : pocketDimension[z]) === null || _a === void 0 ? void 0 : _a[y]) === null || _b === void 0 ? void 0 : _b[x];
+var checkState = function (x, y, z, w) {
+    var _a, _b, _c;
+    var state = (_c = (_b = (_a = pocketDimension === null || pocketDimension === void 0 ? void 0 : pocketDimension[w]) === null || _a === void 0 ? void 0 : _a[z]) === null || _b === void 0 ? void 0 : _b[y]) === null || _c === void 0 ? void 0 : _c[x];
     return state === ACTIVE ? ACTIVE : INACTIVE;
 };
-var checkNeighbors = function (x, y, z) {
+var checkNeighbors = function (x, y, z, w) {
     var activeCount = 0;
-    for (var i = -1; i <= 1; i++) {
-        for (var j = -1; j <= 1; j++) {
-            for (var k = -1; k <= 1; k++) {
-                if (i === 0 && j === 0 && k === 0) {
-                    continue;
-                }
-                var state = checkState(x + k, y + j, z + i);
-                if (state === ACTIVE) {
-                    activeCount++;
+    for (var l = -1; l <= 1; l++) {
+        for (var i = -1; i <= 1; i++) {
+            for (var j = -1; j <= 1; j++) {
+                for (var k = -1; k <= 1; k++) {
+                    if (i === 0 && j === 0 && k === 0 && l === 0) {
+                        continue;
+                    }
+                    var state = checkState(x + k, y + j, z + i, w + l);
+                    if (state === ACTIVE) {
+                        activeCount++;
+                    }
                 }
             }
         }
     }
     return activeCount;
 };
-var safeAdd = function (dimension, x, y, z, value) {
-    if (!dimension[z]) {
-        dimension[z] = [];
+var safeAdd = function (dimension, x, y, z, w, value) {
+    if (!dimension[w]) {
+        dimension[w] = [];
     }
-    if (!dimension[z][y]) {
-        dimension[z][y] = [];
+    if (!dimension[w][z]) {
+        dimension[w][z] = [];
     }
-    dimension[z][y][x] = value;
+    if (!dimension[w][z][y]) {
+        dimension[w][z][y] = [];
+    }
+    dimension[w][z][y][x] = value;
 };
 var CYCLE_COUNT = 6;
 for (var cycle = 0; cycle < CYCLE_COUNT; cycle++) {
     console.log('cycle: ', cycle + 1);
     var newDimension = [];
-    for (var z = bounds.z[0]; z <= bounds.z[1]; z++) {
-        for (var y = bounds.y[0]; y <= bounds.y[1]; y++) {
-            for (var x = bounds.x[0]; x <= bounds.x[1]; x++) {
-                var curState = checkState(x, y, z);
-                var count = checkNeighbors(x, y, z);
-                if (curState === ACTIVE) {
-                    if (count === 2 || count === 3) {
-                        safeAdd(newDimension, x, y, z, ACTIVE); // active
+    for (var w = bounds.w[0]; w <= bounds.w[1]; w++) {
+        for (var z = bounds.z[0]; z <= bounds.z[1]; z++) {
+            for (var y = bounds.y[0]; y <= bounds.y[1]; y++) {
+                for (var x = bounds.x[0]; x <= bounds.x[1]; x++) {
+                    var curState = checkState(x, y, z, w);
+                    var count = checkNeighbors(x, y, z, w);
+                    if (curState === ACTIVE) {
+                        if (count === 2 || count === 3) {
+                            safeAdd(newDimension, x, y, z, w, ACTIVE); // active
+                        }
+                        else {
+                            safeAdd(newDimension, x, y, z, w, INACTIVE); // inactive
+                        }
                     }
-                    else {
-                        safeAdd(newDimension, x, y, z, INACTIVE); // inactive
-                    }
-                }
-                else { // === INACTIVE
-                    if (count === 3) {
-                        safeAdd(newDimension, x, y, z, ACTIVE); // active
-                    }
-                    else {
-                        safeAdd(newDimension, x, y, z, INACTIVE); // inactive
+                    else { // === INACTIVE
+                        if (count === 3) {
+                            safeAdd(newDimension, x, y, z, w, ACTIVE); // active
+                        }
+                        else {
+                            safeAdd(newDimension, x, y, z, w, INACTIVE); // inactive
+                        }
                     }
                 }
             }
@@ -108,15 +116,18 @@ for (var cycle = 0; cycle < CYCLE_COUNT; cycle++) {
     bounds.x = [bounds.x[0] - 1, bounds.x[1] + 1];
     bounds.y = [bounds.y[0] - 1, bounds.y[1] + 1];
     bounds.z = [bounds.z[0] - 1, bounds.z[1] + 1];
+    bounds.w = [bounds.w[0] - 1, bounds.w[1] + 1];
     pocketDimension = newDimension;
 }
 // logDimension(pocketDimension);
 var activeCount = 0;
-for (var z = bounds.z[0]; z <= bounds.z[1]; z++) {
-    for (var y = bounds.y[0]; y <= bounds.y[1]; y++) {
-        for (var x = bounds.x[0]; x <= bounds.x[1]; x++) {
-            if (((_b = (_a = pocketDimension === null || pocketDimension === void 0 ? void 0 : pocketDimension[z]) === null || _a === void 0 ? void 0 : _a[y]) === null || _b === void 0 ? void 0 : _b[x]) === ACTIVE) {
-                activeCount++;
+for (var w = bounds.w[0]; w <= bounds.w[1]; w++) {
+    for (var z = bounds.z[0]; z <= bounds.z[1]; z++) {
+        for (var y = bounds.y[0]; y <= bounds.y[1]; y++) {
+            for (var x = bounds.x[0]; x <= bounds.x[1]; x++) {
+                if (((_c = (_b = (_a = pocketDimension === null || pocketDimension === void 0 ? void 0 : pocketDimension[w]) === null || _a === void 0 ? void 0 : _a[z]) === null || _b === void 0 ? void 0 : _b[y]) === null || _c === void 0 ? void 0 : _c[x]) === ACTIVE) {
+                    activeCount++;
+                }
             }
         }
     }
--- d17/pt2/index.ts
+++ d17/pt2/index.ts
@@ -1,6 +1,6 @@
 import * as fs from 'fs';
 
-const logDimension = (dim: string[][][]) => {
+const logDimension = (dim: string[][][][]) => {
   for (let z = bounds.z[0]; z <= bounds.z[1]; z++) {
     console.log('\nz =', z);
     for (let y = bounds.y[0]; y <= bounds.y[1]; y++) {
@@ -24,30 +24,33 @@ const bounds = {
   x: [-1, data[0].length],
   y: [-1, data.length],
   z: [-1, 1],
+  w: [-1, 1],
 };
 
-let pocketDimension = [data];
+let pocketDimension: string[][][][] = [[data]];
 // logDimension(pocketDimension);
 
-const checkState = (x: number, y: number, z: number) => {
-  const state = pocketDimension?.[z]?.[y]?.[x];
+const checkState = (x: number, y: number, z: number, w: number) => {
+  const state = pocketDimension?.[w]?.[z]?.[y]?.[x];
   return state === ACTIVE ? ACTIVE : INACTIVE;
 }
 
-const checkNeighbors = (x: number, y: number, z: number) => {
+const checkNeighbors = (x: number, y: number, z: number, w: number) => {
   let activeCount = 0;
-  for (let i = -1; i <= 1; i++) {
-    for (let j = -1; j <= 1; j++) {
-      for (let k = -1; k <= 1; k++) {
+  for (let l = -1; l <= 1; l++) {
+    for (let i = -1; i <= 1; i++) {
+      for (let j = -1; j <= 1; j++) {
+        for (let k = -1; k <= 1; k++) {
+          if (i === 0 && j === 0 && k === 0 && l === 0) {
+            continue;
+          }
 
-        if (i === 0 && j === 0 && k === 0) {
-          continue;
+          const state = checkState(x + k, y + j, z + i, w + l);
+          if (state === ACTIVE) {
+            activeCount++;
+          }
         }
 
-        const state = checkState(x + k, y + j, z + i);
-        if (state === ACTIVE) {
-          activeCount++;
-        }
       }
     }
   }
@@ -55,44 +58,50 @@ const checkNeighbors = (x: number, y: number, z: number) => {
   return activeCount;
 }
 
-const safeAdd = (dimension: string[][][], x: number, y: number, z: number, value: string) => {
-  if (!dimension[z]) {
-    dimension[z] = [];
+const safeAdd = (dimension: string[][][][], x: number, y: number, z: number, w: number, value: string) => {
+  if (!dimension[w]) {
+    dimension[w] = [];
   }
 
-  if (!dimension[z][y]) {
-    dimension[z][y] = [];
+  if (!dimension[w][z]) {
+    dimension[w][z] = [];
   }
 
-  dimension[z][y][x] = value;
+  if (!dimension[w][z][y]) {
+    dimension[w][z][y] = [];
+  }
+
+  dimension[w][z][y][x] = value;
 };
 
 const CYCLE_COUNT = 6;
 for (let cycle = 0; cycle < CYCLE_COUNT; cycle++) {
   console.log('cycle: ', cycle + 1)
-  let newDimension: string[][][] = [];
+  let newDimension: string[][][][] = [];
 
-  for (let z = bounds.z[0]; z <= bounds.z[1]; z++) {
-    for (let y = bounds.y[0]; y <= bounds.y[1]; y++) {
-      for (let x = bounds.x[0]; x <= bounds.x[1]; x++) {
+  for (let w = bounds.w[0]; w <= bounds.w[1]; w++) {
+    for (let z = bounds.z[0]; z <= bounds.z[1]; z++) {
+      for (let y = bounds.y[0]; y <= bounds.y[1]; y++) {
+        for (let x = bounds.x[0]; x <= bounds.x[1]; x++) {
 
-        const curState = checkState(x, y, z);
-        const count = checkNeighbors(x, y, z);
+          const curState = checkState(x, y, z, w);
+          const count = checkNeighbors(x, y, z, w);
 
-        if (curState === ACTIVE) {
-          if (count === 2 || count === 3) {
-            safeAdd(newDimension, x, y, z, ACTIVE) // active
-          } else {
-            safeAdd(newDimension, x, y, z, INACTIVE) // inactive
+          if (curState === ACTIVE) {
+            if (count === 2 || count === 3) {
+              safeAdd(newDimension, x, y, z, w, ACTIVE) // active
+            } else {
+              safeAdd(newDimension, x, y, z, w, INACTIVE) // inactive
+            }
+          } else { // === INACTIVE
+            if (count === 3) {
+              safeAdd(newDimension, x, y, z, w, ACTIVE) // active
+            } else {
+              safeAdd(newDimension, x, y, z, w, INACTIVE) // inactive
+            }
           }
-        } else { // === INACTIVE
-          if (count === 3) {
-            safeAdd(newDimension, x, y, z, ACTIVE) // active
-          } else {
-            safeAdd(newDimension, x, y, z, INACTIVE) // inactive
-          }
+
         }
-
       }
     }
   }
@@ -100,17 +109,20 @@ for (let cycle = 0; cycle < CYCLE_COUNT; cycle++) {
   bounds.x = [bounds.x[0] - 1, bounds.x[1] + 1];
   bounds.y = [bounds.y[0] - 1, bounds.y[1] + 1];
   bounds.z = [bounds.z[0] - 1, bounds.z[1] + 1];
+  bounds.w = [bounds.w[0] - 1, bounds.w[1] + 1];
   pocketDimension = newDimension;
 }
 
 // logDimension(pocketDimension);
 
 let activeCount = 0;
-for (let z = bounds.z[0]; z <= bounds.z[1]; z++) {
-  for (let y = bounds.y[0]; y <= bounds.y[1]; y++) {
-    for (let x = bounds.x[0]; x <= bounds.x[1]; x++) {
-      if (pocketDimension?.[z]?.[y]?.[x] === ACTIVE) {
-        activeCount++;
+for (let w = bounds.w[0]; w <= bounds.w[1]; w++) {
+  for (let z = bounds.z[0]; z <= bounds.z[1]; z++) {
+    for (let y = bounds.y[0]; y <= bounds.y[1]; y++) {
+      for (let x = bounds.x[0]; x <= bounds.x[1]; x++) {
+        if (pocketDimension?.[w]?.[z]?.[y]?.[x] === ACTIVE) {
+          activeCount++;
+        }
       }
     }
   }
