5e94738140ca908d42da4812d1fce0c9545d99ba
Brenton Bazemore <brenton.bazemore@slalom.com>
2020-12-19 19:38:04 -0500

    Finish d19 - pt2

--- d19/pt2/dist/index.js
+++ d19/pt2/dist/index.js
@@ -21,29 +21,15 @@ var __importStar = (this && this.__importStar) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 var fs = __importStar(require("fs"));
 var _ = __importStar(require("lodash"));
-// const permutator = (inputArr: any[]): any[][] => {
-//   let result: any[][] = [];
-//   const permute = (arr: any[], m: any[] = []) => {
-//     if (arr.length === 0) {
-//       result.push(m)
-//     } else {
-//       for (let i = 0; i < arr.length; i++) {
-//         let curr = arr.slice();
-//         let next = curr.splice(i, 1);
-//         permute(curr.slice(), m.concat(next))
-//       }
-//     }
-//   }
-//   permute(inputArr)
-//   const uniq = new Set(result);
-//   return result;
-// }
-var permutator = function (ar1, ar2) {
+var permutator = function (ar1, ar2, ar3) {
+    if (ar3 === void 0) { ar3 = ['']; }
     var out = [];
     for (var i = 0; i < ar1.length; i++) {
         for (var j = 0; j < ar2.length; j++) {
-            var str = "" + ar1[i] + ar2[j];
-            out.push(str);
+            for (var k = 0; k < ar3.length; k++) {
+                var str = "" + ar1[i] + ar2[j] + ar3[k];
+                out.push(str);
+            }
         }
     }
     return out;
@@ -57,8 +43,23 @@ rawRules.split('\n').forEach(function (rawRule) {
     rules[id] = subRules;
 });
 var messages = rawMessages.split('\n');
+var seenParentCount = 0;
+var curParent = null;
 var knownCombos = new Map();
-var findAllCombos = function (ruleId) {
+var findAllCombos = function (ruleId, parent) {
+    if (parent === curParent) {
+        if (seenParentCount > 5) {
+            console.log('reached the depth');
+            return [''];
+        }
+        else {
+            seenParentCount++;
+        }
+    }
+    else {
+        seenParentCount = 1;
+    }
+    curParent = parent;
     if (knownCombos.has(ruleId)) {
         return knownCombos.get(ruleId);
     }
@@ -71,12 +72,12 @@ var findAllCombos = function (ruleId) {
         var splitRules = subRule.split(' ');
         for (var i = 0; i < splitRules.length; i++) {
             var rule = splitRules[i];
-            var combos = findAllCombos(rule);
+            var combos = findAllCombos(rule, ruleId);
             subSubCombos.push(combos);
         }
         var perms = subSubCombos[0];
         if (subSubCombos.length > 1) {
-            perms = permutator(subSubCombos[0], subSubCombos[1]);
+            perms = permutator(subSubCombos[0], subSubCombos[1], subSubCombos[2]);
         }
         return perms;
     });
@@ -84,11 +85,88 @@ var findAllCombos = function (ruleId) {
     knownCombos.set(ruleId, out);
     return out;
 };
-var validCombos = findAllCombos('0');
-var count = 0;
-messages.forEach(function (message) {
-    if (validCombos.includes(message)) {
-        count++;
+var vc31 = Array.from(new Set(findAllCombos('31', null)));
+var vc42 = Array.from(new Set(findAllCombos('42', null)));
+var check42 = function (message) {
+    var newMessage = message;
+    var has = vc42.some(function (combo) {
+        if (message.startsWith(combo)) {
+            newMessage = message.replace(combo, '');
+            return true;
+        }
+        return false;
+    });
+    return { has: has, message: newMessage };
+};
+var check42e = function (message) {
+    var newMessage = message;
+    var has = vc42.some(function (combo) {
+        if (message.endsWith(combo)) {
+            newMessage = message.replace(new RegExp(combo + '$'), '');
+            return true;
+        }
+        return false;
+    });
+    return { has: has, message: newMessage };
+};
+var check31 = function (message) {
+    var newMessage = message;
+    var has = vc31.some(function (combo) {
+        if (message.endsWith(combo)) {
+            newMessage = message.replace(new RegExp(combo + '$'), '');
+            return true;
+        }
+        return false;
+    });
+    return { has: has, message: newMessage };
+};
+;
+var candidates = [];
+for (var i = 0; i < messages.length; i++) {
+    var message = messages[i];
+    var c42 = check42(message);
+    var c31 = check31(message);
+    if (c42.has && c31.has) {
+        candidates.push({ strip31: 0, strip42e: 0, message: message });
+    }
+}
+var newMessages = candidates;
+while (true) {
+    var leftovers = [];
+    for (var i = 0; i < newMessages.length; i++) {
+        var message = newMessages[i];
+        var count = message.strip31;
+        var c31 = check31(message.message);
+        if (c31.has) {
+            count++;
+        }
+        leftovers.push({ strip31: count, strip42e: message.strip42e, message: c31.message });
+    }
+    if (_.isEqual(leftovers, newMessages)) {
+        break;
+    }
+    newMessages = leftovers;
+}
+while (true) {
+    var leftovers = [];
+    for (var i = 0; i < newMessages.length; i++) {
+        var message = newMessages[i];
+        var count = message.strip42e;
+        var c42e = check42e(message.message);
+        if (c42e.has) {
+            count++;
+        }
+        leftovers.push({ strip31: message.strip31, strip42e: count, message: c42e.message });
+    }
+    if (_.isEqual(leftovers, newMessages)) {
+        break;
+    }
+    newMessages = leftovers;
+}
+var c = 0;
+newMessages.forEach(function (m) {
+    if (m.message === '' && m.strip42e > m.strip31) {
+        c++;
     }
 });
-console.log(count);
+console.log(c);
--- d19/pt2/index.ts
+++ d19/pt2/index.ts
@@ -1,12 +1,14 @@
 import * as fs from 'fs';
 import * as _ from 'lodash';
 
-const permutator = (ar1: string[], ar2: string[]) => {
+const permutator = (ar1: string[], ar2: string[], ar3: string[] = ['']) => {
   const out = [];
   for (let i = 0; i < ar1.length; i++) {
     for (let j = 0; j < ar2.length; j++) {
-      const str = `${ar1[i]}${ar2[j]}`;
-      out.push(str);
+      for (let k = 0; k < ar3.length; k++) {
+        const str = `${ar1[i]}${ar2[j]}${ar3[k]}`;
+        out.push(str);
+      }
     }
   }
 
@@ -26,8 +28,23 @@ rawRules.split('\n').forEach((rawRule) => {
 
 const messages = rawMessages.split('\n');
 
+let seenParentCount = 0;
+let curParent: any = null;
 const knownCombos = new Map();
-const findAllCombos = (ruleId: string): string[] => {
+const findAllCombos = (ruleId: string, parent: any): string[] => {
+  if (parent === curParent) {
+    if (seenParentCount > 5) {
+      console.log('reached the depth')
+      return [''];
+    } else {
+      seenParentCount++;
+    }
+  } else {
+    seenParentCount = 1;
+  }
+
+  curParent = parent;
+
   if (knownCombos.has(ruleId)) {
     return knownCombos.get(ruleId);
   }
@@ -42,13 +59,13 @@ const findAllCombos = (ruleId: string): string[] => {
     const splitRules = subRule.split(' ');
     for (let i = 0; i < splitRules.length; i++) {
       const rule = splitRules[i];
-      const combos = findAllCombos(rule);
+      const combos = findAllCombos(rule, ruleId);
       subSubCombos.push(combos);
     }
 
     let perms = subSubCombos[0];
     if (subSubCombos.length > 1) {
-      perms = permutator(subSubCombos[0], subSubCombos[1]);
+      perms = permutator(subSubCombos[0], subSubCombos[1], subSubCombos[2]);
     }
 
     return perms;
@@ -60,13 +77,117 @@ const findAllCombos = (ruleId: string): string[] => {
   return out;
 }
 
-const validCombos = findAllCombos('0');
+const vc31 = Array.from(new Set(findAllCombos('31', null)));
+const vc42 = Array.from(new Set(findAllCombos('42', null)));
 
-let count = 0;
-messages.forEach(message => {
-  if (validCombos.includes(message)) {
-    count++;
+const check42 = (message: string) => {
+  let newMessage = message;
+  const has = vc42.some((combo) => {
+    if (message.startsWith(combo)) {
+      newMessage = message.replace(combo, '');
+      return true;
+    }
+
+    return false;
+  });
+
+  return { has, message: newMessage };
+}
+const check42e = (message: string) => {
+  let newMessage = message;
+  const has = vc42.some((combo) => {
+    if (message.endsWith(combo)) {
+      newMessage = message.replace(new RegExp(combo + '$'), '');
+      return true;
+    }
+
+    return false;
+  });
+
+  return { has, message: newMessage };
+}
+
+const check31 = (message: string) => {
+  let newMessage = message;
+  const has = vc31.some((combo) => {
+    if (message.endsWith(combo)) {
+      newMessage = message.replace(new RegExp(combo + '$'), '');
+      return true;
+    }
+
+    return false;
+  });
+
+  return { has, message: newMessage };
+}
+
+interface StripTracker { strip31: number, strip42e: number, message: string };
+
+const candidates: StripTracker[] = [];
+
+for (let i = 0; i < messages.length; i++) {
+  const message = messages[i];
+
+  let c42 = check42(message);
+  let c31 = check31(message);
+
+  if (c42.has && c31.has) {
+    candidates.push({ strip31: 0, strip42e: 0, message });
   }
-});
+}
 
-console.log(count);
+let newMessages = candidates;
+
+
+while (true) {
+  const leftovers: StripTracker[] = [];
+  for (let i = 0; i < newMessages.length; i++) {
+    const message = newMessages[i];
+
+    let count = message.strip31;
+    let c31 = check31(message.message);
+    if (c31.has) {
+      count++;
+    }
+
+    leftovers.push({ strip31: count, strip42e: message.strip42e, message: c31.message });
+  }
+
+
+  if (_.isEqual(leftovers, newMessages)) {
+    break;
+  }
+
+  newMessages = leftovers;
+}
+
+while (true) {
+  const leftovers: StripTracker[] = [];
+  for (let i = 0; i < newMessages.length; i++) {
+    const message = newMessages[i];
+
+    let count = message.strip42e;
+    let c42e = check42e(message.message);
+    if (c42e.has) {
+      count++;
+    }
+
+    leftovers.push({ strip31: message.strip31, strip42e: count, message: c42e.message });
+  }
+
+
+  if (_.isEqual(leftovers, newMessages)) {
+    break;
+  }
+
+  newMessages = leftovers;
+}
+
+let c = 0;
+newMessages.forEach(m => {
+  if (m.message === '' && m.strip42e > m.strip31) {
+    c++;
+  }
+})
+
+console.log(c);
