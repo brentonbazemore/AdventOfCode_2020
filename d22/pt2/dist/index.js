12341b841ea3eddbc2de9d5485362c1504c0013e
Brenton Bazemore <brenton.bazemore@slalom.com>
2020-12-22 14:12:18 -0500

    Finish d22 - pt2

--- d22/pt2/dist/index.js
+++ d22/pt2/dist/index.js
@@ -18,6 +18,13 @@ var __importStar = (this && this.__importStar) || function (mod) {
     __setModuleDefault(result, mod);
     return result;
 };
+var __spreadArrays = (this && this.__spreadArrays) || function () {
+    for (var s = 0, i = 0, il = arguments.length; i < il; i++) s += arguments[i].length;
+    for (var r = Array(s), k = 0, i = 0; i < il; i++)
+        for (var a = arguments[i], j = 0, jl = a.length; j < jl; j++, k++)
+            r[k] = a[j];
+    return r;
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 var fs = __importStar(require("fs"));
 var rawData = fs.readFileSync('input.txt', 'utf8');
@@ -36,18 +43,53 @@ var Player = /** @class */ (function () {
     };
     return Player;
 }());
+var playWar = function (p1, p2) {
+    var snapshots = new Set();
+    var winner = null;
+    var round = 1;
+    while (p1.deck.length > 0 && p2.deck.length > 0) {
+        var roundSnap = [p1.deck.join(','), p2.deck.join(',')].join(':');
+        if (snapshots.has(roundSnap)) {
+            return { player: p1, id: 1 };
+        }
+        snapshots.add([p1.deck.join(','), p2.deck.join(',')].join(':'));
+        var p1Card = p1.getCard();
+        var p2Card = p2.getCard();
+        // console.log({ round, p1: { card: p1Card, deck: p1.deck }, p2: { card: p2Card, deck: p2.deck } });
+        if (p1.deck.length >= p1Card && p2.deck.length >= p2Card) {
+            var fullP1 = __spreadArrays([p1Card], p1.deck);
+            var fullP2 = __spreadArrays([p2Card], p2.deck);
+            var newP1 = new Player(fullP1.slice(0, p1Card + 1).join('\n'));
+            var newP2 = new Player(fullP2.slice(0, p2Card + 1).join('\n'));
+            winner = playWar(newP1, newP2);
+            if (winner.id === 1) {
+                p1.addWinnings(p1Card, p2Card);
+            }
+            else if (winner.id === 2) {
+                p2.addWinnings(p2Card, p1Card);
+            }
+        }
+        else {
+            if (p1Card > p2Card) {
+                p1.addWinnings(p1Card, p2Card);
+            }
+            else if (p2Card > p1Card) {
+                p2.addWinnings(p2Card, p1Card);
+            }
+        }
+        round++;
+    }
+    if (p1.deck.length === 0) {
+        return { player: p2, id: 2 };
+    }
+    if (p2.deck.length === 0) {
+        return { player: p1, id: 1 };
+    }
+    return { player: p1, id: 1 };
+};
 var player1 = new Player(p1String);
 var player2 = new Player(p2String);
-while (player1.deck.length > 0 && player2.deck.length > 0) {
-    var p1Card = player1.getCard();
-    var p2Card = player2.getCard();
-    if (p1Card > p2Card) {
-        player1.addWinnings(p1Card, p2Card);
-    }
-    else if (p2Card > p1Card) {
-        player2.addWinnings(p2Card, p1Card);
-    }
-}
+var winner = playWar(player1, player2);
 var calculateScore = function (p) {
     var deckSize = p.deck.length;
     var sum = 0;
@@ -56,5 +98,4 @@ var calculateScore = function (p) {
     }
     console.log(sum);
 };
-calculateScore(player1);
-calculateScore(player2);
+calculateScore(winner.player);
--- d22/pt2/index.ts
+++ d22/pt2/index.ts
@@ -20,19 +20,58 @@ class Player {
   }
 }
 
+const playWar = (p1: Player, p2: Player): { player: Player, id: number } => {
+  const snapshots = new Set();
+  let winner = null;
+  let round = 1;
+  while (p1.deck.length > 0 && p2.deck.length > 0) {
+    const roundSnap = [p1.deck.join(','), p2.deck.join(',')].join(':');
+    if (snapshots.has(roundSnap)) {
+      return { player: p1, id: 1 };
+    }
+
+    snapshots.add([p1.deck.join(','), p2.deck.join(',')].join(':'));
+
+    const p1Card = p1.getCard();
+    const p2Card = p2.getCard();
+
+    if (p1.deck.length >= p1Card && p2.deck.length >= p2Card) {
+      const fullP1 = [p1Card, ...p1.deck];
+      const fullP2 = [p2Card, ...p2.deck];
+      const newP1 = new Player(fullP1.slice(0, p1Card + 1).join('\n'));
+      const newP2 = new Player(fullP2.slice(0, p2Card + 1).join('\n'));
+      winner = playWar(newP1, newP2);
+
+      if (winner.id === 1) {
+        p1.addWinnings(p1Card, p2Card);
+      } else if (winner.id === 2) {
+        p2.addWinnings(p2Card, p1Card);
+      }
+    } else {
+      if (p1Card > p2Card) {
+        p1.addWinnings(p1Card, p2Card);
+      } else if (p2Card > p1Card) {
+        p2.addWinnings(p2Card, p1Card);
+      }
+    }
+
+    round++;
+  }
+
+  if (p1.deck.length === 0) {
+    return { player: p2, id: 2 };
+  }
+
+  if (p2.deck.length === 0) {
+    return { player: p1, id: 1 };
+  }
+
+  return { player: p1, id: 1 };
+}
+
 const player1 = new Player(p1String);
 const player2 = new Player(p2String);
-
-while (player1.deck.length > 0 && player2.deck.length > 0) {
-  const p1Card = player1.getCard();
-  const p2Card = player2.getCard();
-
-  if (p1Card > p2Card) {
-    player1.addWinnings(p1Card, p2Card);
-  } else if (p2Card > p1Card) {
-    player2.addWinnings(p2Card, p1Card);
-  }
-}
+const winner = playWar(player1, player2);
 
 const calculateScore = (p: Player) => {
   const deckSize = p.deck.length;
@@ -43,5 +82,4 @@ const calculateScore = (p: Player) => {
   console.log(sum);
 }
 
-calculateScore(player1);
-calculateScore(player2);
+calculateScore(winner.player);
